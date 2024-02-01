f2ee21654b7535a97285be917e61c2d1de104c1e
Florian Hye <31217036+Flo2410@users.noreply.github.com>
2024-02-01 02:17:42 +0100

    Feature: search suggestions for search and quick launch (#2775)
    ---------
    
    Co-authored-by: shamoon <4887959+shamoon@users.noreply.github.com>

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -218,12 +218,12 @@ function Home({ initialSettings, authContext }) {
       // if search provider is a list, try to retrieve from localstorage, fall back to the first
       searchProvider = getStoredProvider() ?? searchProviders[searchWidget.options.provider[0]];
     } else if (searchWidget.options?.provider === "custom") {
-      searchProvider = {
-        url: searchWidget.options.url,
-      };
+      searchProvider = searchWidget.options;
     } else {
       searchProvider = searchProviders[searchWidget.options?.provider];
     }
+    // to pass to quicklaunch
+    searchProvider.showSearchSuggestions = searchWidget.options?.showSearchSuggestions;
   }
   const headerStyle = settings?.headerStyle || "underlined";
 
